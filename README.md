b53d3a7cc8a15b36241bad6a9ea976a7d80c0828
ChrisxLiz <chrislizi@gmail.com>
2023-11-20 22:37:47 -0600

    nueva ui

--- README.md
+++
@@ -1,1 +0,0 @@
-# nocturna_pi
--- index.php
+++ index.php
@@ -1,79 +1,52 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <title>Citas Médicas | Clinica London</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="shortcut icon" href="images/sv.svg" type="image/x-icon">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,700;1,900&display=swap" rel="stylesheet">
-    <!-- Bootstrap CSS v5.2.1 -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-    <link rel="stylesheet" href="/css/index.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CITAS MEDICAS</title>
+    <link rel="stylesheet" href="css/main.css">
 </head>
-
-<body class="body">
-    <header id="header">
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark p-4">
-            <a class="navbar-brand" href="#">Clinica X</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse derecha" id="navbarNav">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#header">NOSOTROS</a>
+<body>
+    <header class="header__section">
+        <div class="nav__container">
+            <nav class="nav">
+                <img class="logo" src="/images/logo.png" alt="">
+                <ul class="nav__list">
+                    <li>
+                        <a href="#">Inicio</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#main">SERVICIOS</a>
+                    <li>
+                        <a href="#">Nosotros</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#contacto">CONTACTO</a>
+                    <li>
+                        <a href="#">Especialidades</a>
                     </li>
-                </ul>
-                <ul class="navbar-nav">
-                    <li class="nav-item mx-2">
-                        <a class="btn btn-success" href="#">Login</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="btn btn-primary" href="#">Crear cuenta</a>
+                    <li>
+                        <a href="#">Contacto</a>
                     </li>
                 </ul>
+                <ul class="nav__list">
+                    <li>
+                        <a href="#" class="boton auth">Login</a>
+                    </li>
+                    <li>
+                        <a href="#" class="boton auth">Register</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+        <div class="header__info">
+            <div class="header__image">
+                <img src="/images/img-1.png" alt="">
             </div>
-        </nav>
-        <div class="imagen text-white">
-            <div class="imagen__info">
-                <div class="text-center">
-                    <h1 class="">CITAS MÉDICAS</h1>
-                    <h2>El mejor servicio de Medicina en la Ciudad de Quito.</h2>
-                </div>
+            <div class="header__text">
+                <h1>CITAS MEDICAS</h1>
+                <p>Nuestro servicio está disponible en la ciudad de Quito.</p>
+                <p>Para agendar una cita deber crear una cuenta!</p>
+                <a href="#" class="boton cuenta">Crear cuenta</a>
             </div>
         </div>
-
     </header>
-    <main id="main" class="main">
-        <h1>Nuestros Servicios</h1>
-        <div class="servi d-flex justify-content-between p-5">
-            <div class="imagenes__servicios">
-                <h1>img servicios</h1>
-            </div>
-            <div class="info__servicios">
-                <h1>INFO SERVICIOS</h1>
-            </div>
-        </div>
-    </main>
-    <footer id="contacto" class="footer">
-        <!-- place footer here -->
-    </footer>
-    <!-- Bootstrap JavaScript Libraries -->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
-    </script>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
-    </script>
 </body>
-
 </html>
