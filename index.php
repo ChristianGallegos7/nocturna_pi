5a2817017c4586320939e8b822ee3b628f500ad4
Christian <christiantenz7@gmail.com>
2023-11-22 14:54:51 -0500

    Haciendo el contacto

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
                         <a href="#main">Especialidades</a>
                     </li>
                     <li>
-                        <a href="#">Contacto</a>
+                        <a href="#contacto">Contacto</a>
                     </li>
                 </ul>
                 <ul class="nav__list">
@@ -47,25 +47,32 @@
         </div>
     </header>
     <main class="main__section" id="main">
-        <h1>Especialidades</h1>
         <div class="especialidades__info">
-            <div class="especialidades__text">
-                <h2>Especialidades Disponibles</h2>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta illum voluptatem maiores totam consectetur officiis repellendus. Voluptatibus harum quo impedit!</p>
+            <h1>Especialidades</h1>
+            <div class="especialidades">
+                <div class="especialidades__text">
+                    <h2>Especialidades Disponibles</h2>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta illum voluptatem maiores totam consectetur officiis repellendus. Voluptatibus harum quo impedit!</p>
+                </div>
+                <div class="especialidades__images">
+                    <div class="corazon">
+                        <h3>Corazón</h3>
+                        <img src="images/icon-5.png" class="esp esp_corazon" alt="Especialidad 1">
+                    </div>
+                    <div class="fracturas">
+                        <h3>Fracturas</h3>
+                        <img src="images/icon-4.png" class="esp esp_fracturas" alt="Especialidad 2">
+                    </div>
+                    <div class="examenes">
+                        <h3>Examenes</h3>
+                        <img src="images/icon-6.png" class="esp esp_examenes" alt="Especialidad 3">
+                    </div>
+                </div>
             </div>
-            <div class="especialidades__images">
-                <div class="corazon">
-                    <h3>Corazón</h3>
-                    <img src="images/icon-5.png" class="esp esp_corazon" alt="Especialidad 1">
-                </div>
-                <div class="fracturas">
-                    <h3>Fracturas</h3>
-                    <img src="images/icon-4.png" class="esp esp_fracturas" alt="Especialidad 2">
-                </div>
-                <div class="examenes">
-                    <h3>Examenes</h3>
-                    <img src="images/icon-6.png" class="esp esp_examenes" alt="Especialidad 3">
-                </div>
+        </div>
+        <div class="contacto" id="contacto">
+            <div class="contacto__section">
+
             </div>
         </div>
     </main>
