405247a4042159dea06b7064342f7966ff86d95c
unknown <chris@chris.com>
2023-12-15 22:16:21 -0500

    trabajando en el doctor

--- admin/doctores/crear.php
+++ admin/doctores/crear.php
@@ -1,46 +1,38 @@
 <?php
-
-//base de datos
-require "./includes/config/database.php";
-
+    include "../templates/header.php";
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulario Doctores</title>
-</head>
-<body>
     <main>
         <h1>Crear Doctor</h1>
 
-        <form action="" method="POST">
-            <div>
-                <label for="foto">Foto:</label>
-                <input type="file" name="foto" id="foto">
-            </div>
-            <div>
-                <label for="nombre">Nombre</label>
-                <input type="text" name="nombre" id="nombre">
-            </div>
-            <div>
-                <label for="apellido">Apellido</label>
-                <input type="text" name="apellido" id="apellido">
-            </div>
-            <div>
-                <label for="especialidad">Especialidad</label>
-                <input type="text" name="especialidad" id="especialidad">
+        <form action="" method="POST" enctype="" class="formulario">
+            <fieldset>
+                <legend>Información General</legend>
+                <div>
+                    <label for="foto">Foto:</label>
+                    <input type="file" name="foto" id="foto">
+                </div>
+                <div>
+                    <label for="nombre">Nombre:</label>
+                    <input type="text" name="nombre" id="nombre">
+                </div>
+                <div>
+                    <label for="apellido">Apellido:</label>
+                    <input type="text" name="apellido" id="apellido">
+                </div>
+                <div>
+                    <label for="especialidad">Especialidad:</label>
+                    <input type="text" name="especialidad" id="especialidad">
 
-            </div>
-            <div>
-                <label for="horario">Horario</label>
-                <input type="text" name="horario" id="horario">
-            </div>
+                </div>
+                <div>
+                    <label for="horario">Horario:</label>
+                    <input type="text" name="horario" id="horario">
+                </div>
 
-            <button type="submit">Crear</button>
+                <button type="submit" class="btn-verde">Añadir Doctor</button>
+            </fieldset>
         </form>
     </main>
 </body>
+
 </html>
---
+++ admin/templates/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CITAS MEDICAS</title>
+    <link rel="stylesheet" href="../../build/css/app.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700;900&display=swap" rel="stylesheet">
+
+    <style>
+        .header_admin{
+            background-color: #16E4D1;
+        }
+    </style>
+</head>
+
+<body class="animate__animated animate__fadeIn">
+
+    <header class="header_admin header__section" id="header">
+        <div class="nav__container">
+            <nav class="nav animate__animated animate__fadeInDown">
+                <img class="logo animate__animated animate__fadeInLeft" src="../../build/img/logo.webp" alt="ayudae">
+               
+                <ul class="nav__list animate__animated animate__fadeIn derecha">
+                    <li>
+                        <a href="../index.php" class=" auth animate__animated animate__fadeInRight">Admin</a>
+                    </li>
+                    <li>
+                        <a href="./views/auth/register.php" class=" auth animate__animated animate__fadeInRight">Register</a>
+                    </li>
+                </ul>
+            </nav>
+    </header>
